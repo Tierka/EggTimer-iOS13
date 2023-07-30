9cbf5ed99b92f571e487f6fcfa59d2d8bc2e824b
Mariia Ovchinnikova <ghostlush@gmail.com>
2023-07-30 21:10:02 +0300

    add timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var timer = Timer()
+    var letTime: Int = 0
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 9]
+    var second = 0
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+    }
 
-
+    @IBAction func hardnessSelector(_ sender: UIButton) {
+        timer.invalidate()
+        let hardnessEgg = sender.currentTitle!
+        letTime = eggTimes[hardnessEgg]!
+        timer  = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func startTimer(){
+        
+        if second < letTime {
+            second += 1
+            print("timer is: \(letTime - second)")
+        } else {
+            timer.invalidate()
+        }
+        
+    }
 }
